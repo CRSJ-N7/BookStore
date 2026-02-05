5aaa98c65939db608bf6e636e67f9a64d8408293
CRSJ <crossjawz@gmail.com>
2026-02-05 16:48:38 +0300

    Footer added

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #0d1821;
+  height: 341px;
+  padding: 48px 64px;
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  p {
+    color: #f0f4ef;
+  }
+`;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import { BaseLogo, BaseParagraph } from "../../shared/styles/styles";
+import footerLogo from "../../assets/footer/footer-logo.png";
+import map from "../../assets/footer/map.png";
+import { InfoWrapper, FooterWrapper } from "./Footer.styles";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <InfoWrapper>
+        <BaseLogo src={footerLogo} alt="logo" />
+        <BaseParagraph style={{ marginTop: "25px" }}>
+          tranthuy.nute@gmail.com
+        </BaseParagraph>
+        <BaseParagraph>(480) 555-0103</BaseParagraph>
+      </InfoWrapper>
+
+      <InfoWrapper>
+        <BaseParagraph>Home page</BaseParagraph>
+        <BaseParagraph>Catalog</BaseParagraph>
+        <BaseParagraph>My account</BaseParagraph>
+        <BaseParagraph>Cart</BaseParagraph>
+      </InfoWrapper>
+
+      <InfoWrapper>
+        <BaseParagraph>6391 Elgin St. Celina, Delaware 10299</BaseParagraph>
+        <img src={map} alt="logo"></img>
+      </InfoWrapper>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
